252aeac7002772f88a87348e99ef69fa43df2fa1
Gabriel Cor <gabriel.cor@beacon42.com>
2023-12-08 16:59:47 -0300

    code cleanup

--- components/knock_pattern_detector/knock_pattern_detector.cpp
+++ components/knock_pattern_detector/knock_pattern_detector.cpp
@@ -5,26 +5,7 @@ namespace knock_pattern_detector {
 static const char *const TAG = "knock_pattern_detector";
 
 void CustomKnockPatternDetector::setup() {
-  /*
-      ESP_LOGD("CustomKnockSensor", "Pin: %d", this->pin_);
-
-      ESP_LOGD("CustomKnockSensor", "Knock Pattern Length: %d", knock_pattern_length_);
-
-      ESP_LOGD("CustomKnockSensor", "Knock Sensor Threshold: %d", knock_sensor_threshold_);
-
-      ESP_LOGD("CustomKnockSensor", "Knock Error Tolerance: %d", knock_error_tolerance_);
-
-      ESP_LOGD("CustomKnockSensor", "Knock Average Error Tolerance: %d", knock_average_error_tolerance_);
-
-      ESP_LOGD("CustomKnockSensor", "Knock Pattern Min Gap: %d", knock_pattern_mingap_between_knocks_);
-
-      ESP_LOGD("CustomKnockSensor", "Knock Pattern Max Gap: %d", knock_pattern_maxgap_between_knocks_);
-
-      ESP_LOGD("CustomKnockSensor", "Knock Pattern Values:");
-      for (const auto &value : knock_pattern_) {
-          ESP_LOGD("CustomKnockSensor", "%d", value);
-      }
-      */
+ 
 }
 
 void CustomKnockPatternDetector::dump_config() {
